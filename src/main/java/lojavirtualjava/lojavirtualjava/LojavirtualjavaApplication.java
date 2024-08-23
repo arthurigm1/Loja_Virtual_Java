dd33ca443021abb1e269aa879f001814fb165176
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2024-08-22 23:43:10 -0300

    aa

---
+++ src/main/java/lojavirtualjava/lojavirtualjava/LojavirtualjavaApplication.java
@@ -0,0 +1,13 @@
+package lojavirtualjava.lojavirtualjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lojavirtualjava/lojavirtualjava/LojavirtualjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package lojavirtualjava.lojavirtualjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
